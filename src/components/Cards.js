168f6a93be4235a5143f30a2aa93b9751e2edde2
enwachukwu <enwachukwu869@gmail.com>
2022-12-17 01:02:10 +0000

    first commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-        <h1></h1>
+      
       <div className='cards__container'>
         <div className='cards__wrapper'>
             <ul className='cards__items'>
@@ -18,7 +18,7 @@ function Cards() {
                 <CardItem
                 src="images/img_6.jpg" text="This is a testing page, we would write the main text sonner"
                  label="Happy day"
-                path="/ContactUs"
+                path="/AboutUs"
                 />
              
             </ul>
